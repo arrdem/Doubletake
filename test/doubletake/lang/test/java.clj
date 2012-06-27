8fc1b34e26e176b398b8319438ff9a8bd90110f7
rmckenzie <rmckenzie92@gmail.com>
2012-06-27 10:54:24 -0500

    Cleaned up tests from a single loop into individual tests but needs work

--- test/doubletake/lang/test/java.clj
+++ test/doubletake/lang/test/java.clj
@@ -5,19 +5,42 @@
     [doubletake.lang.java.syntax :as jsyntax]
     [clojure.test]))
 
-(def test-strings
-  ["class Point { int x, y; }\n"
-   "package vista;\nclass Point { int x, y; }\n"
-   "package vista;\nimport java.util.Vector;\nclass Point { int x, y; }\n"])
+(defmacro java-parser-test [test-name string]
+  (deftest test-name 
+           (quote
+             (println "-------------------------------------------------")
+             (print string "\n\n")
+             (let [res (jcore/parse string)]
+               (is (not (= nil res)))
+               (pprint res)))))
 
-(deftest string-parse
-  (loop [cur  (first test-strings)
-         tail (rest test-strings)]
-    (let [res (jcore/parse cur)]
-      (is (not (= nil res)))
-      (println "-------------------------------------------------")
-      (print cur "\n\n")
-      (pprint res)
-      (if (not (empty? tail))
-        (recur (first tail) (rest tail))))))
+(java-parser-test basic-package "package Foo;\n")
 
+
+(java-parser-test package-with-import 
+                  (str "package Foo;\n"
+                       "import Clojure.Awesome.Functional;\n"))
+
+(java-parser-test package-with-wildcard-import 
+                 (str "package Foo;\n"
+                      "import Clojure.Awesome.Functional;\n"
+                      "import Foo.Bar.Baz.*;\n"))
+
+
+(java-parser-test empty-class (str "class Bar {}\n"))
+
+(java-parser-test class-with-modified-fields 
+                 (str "class Bar {\n"
+                      "public int a;\n"
+                      "protected int b;\n"
+                      "private static int i;\n"
+                      "}\n"))
+
+
+(java-parser-test class-with-member-function 
+                  (str "class A_Stupid_Name {\n"
+                       "    public int tarded;\n"
+                       "    public A_Stupid_Name() {\n"
+                       "        this.tarded = 5;\n"
+                       "    }\n"
+                       "}\n"))

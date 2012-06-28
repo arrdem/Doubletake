7cefec3b0bce6078e1d70fcc43caae05b75fbda3
rmckenzie <rmckenzie92@gmail.com>
2012-06-28 10:54:50 -0500

    Rework tests to use macro expansion templating

--- test/doubletake/lang/test/java.clj
+++ test/doubletake/lang/test/java.clj
@@ -5,42 +5,41 @@
     [doubletake.lang.java.syntax :as jsyntax]
     [clojure.test]))
 
-(defmacro java-parser-test [test-name string]
-  (deftest test-name 
-           (quote
-             (println "-------------------------------------------------")
-             (print string "\n\n")
-             (let [res (jcore/parse string)]
-               (is (not (= nil res)))
-               (pprint res)))))
+(defmacro java-parser-test [string]
+  ;`(print ~string "\n\n")
+  `(is (not (nil? (jcore/parse ~string)))))
 
-(java-parser-test basic-package "package Foo;\n")
+(deftest basic-package
+  (java-parser-test "package Foo;\n"))
 
+(deftest package-with-import
+  (java-parser-test
+    (str "package Foo;\n"
+         "import Clojure.Awesome.Functional;\n")))
 
-(java-parser-test package-with-import 
-                  (str "package Foo;\n"
-                       "import Clojure.Awesome.Functional;\n"))
+(deftest package-with-wildcard-import 
+  (java-parser-test
+    (str "package Foo;\n"
+         "import Clojure.Awesome.Functional;\n"
+         "import Foo.Bar.Baz.*;\n")))
 
-(java-parser-test package-with-wildcard-import 
-                 (str "package Foo;\n"
-                      "import Clojure.Awesome.Functional;\n"
-                      "import Foo.Bar.Baz.*;\n"))
+(deftest empty-class
+  (java-parser-test 
+    (str "class Bar {}\n")))
 
+(deftest class-with-modified-fields 
+  (java-parser-test 
+    (str "class Bar {\n"
+         "public int a;\n"
+         "protected int b;\n"
+         "private static int i;\n"
+         "}\n")))
 
-(java-parser-test empty-class (str "class Bar {}\n"))
-
-(java-parser-test class-with-modified-fields 
-                 (str "class Bar {\n"
-                      "public int a;\n"
-                      "protected int b;\n"
-                      "private static int i;\n"
-                      "}\n"))
-
-
-(java-parser-test class-with-member-function 
-                  (str "class A_Stupid_Name {\n"
-                       "    public int tarded;\n"
-                       "    public A_Stupid_Name() {\n"
-                       "        this.tarded = 5;\n"
-                       "    }\n"
-                       "}\n"))
+(deftest class-with-member-function
+  (java-parser-test
+    (str "class A_Stupid_Name {\n"
+         "    public int tarded;\n"
+         "    public A_Stupid_Name() {\n"
+         "        this.tarded = 5;\n"
+         "    }\n"
+         "}\n")))

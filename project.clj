e5e6bf248fc7e62c6fbef199779019acbab3a0e1
rmckenzie <rmckenzie92@gmail.com>
2012-07-02 16:54:15 -0500

    Merge and update project deps

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject doubletake "1.0.0-SNAPSHOT"
   :description "A semantic interference detector for Java"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [fnparse "2.2.7"]
-                 [ordered "1.3.0"]]
-  :source-paths ["src/"]
-  :javac-source-paths ["src/"]
-  :javac-options {:debug "true"}
-  :main doubletake.java.spec.core)
+ :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojars.doo/fnparse "2.2.8"]
+                 [lein-javac "1.3.0"]
+                 [clojure-contrib "1.2.0"]
+                 [lein-junit "1.0.0"]]
+   :source-paths ["src/"]
+   :javac-source-paths ["src/" "test/"]
+   :javac-options {:debug "true"}
+   :junit [["classes"]]
+   :main doubletake.core)

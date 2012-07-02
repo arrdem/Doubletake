ea9782c9fc798e7eb07e4993eb47bd01703d7315
rmckenzie <rmckenzie92@gmail.com>
2012-07-02 16:40:18 -0500

    Fixed the project file

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                  [lein-javac "1.3.0"]
                  [lein-junit "1.0.0"]]
    :source-paths ["src/"]
-   :javac-source-paths ["src/"]
+   :javac-source-paths ["src/" "test/"]
    :javac-options {:debug "true"}
+   :junit [["classes"]]
    :main doubletake.core)
 

884bae36916089f9af1d02ef8bd1401da48ede13
rmckenzie <rmckenzie92@gmail.com>
2012-07-02 16:40:43 -0500

    Add trivial argument printing code

--- src/doubletake/core.clj
+++ src/doubletake/core.clj
@@ -1,2 +1,17 @@
 (ns doubletake.core
-  :use [doubletake.parser])
+  (:use [doubletake.lang.java.core]
+        [clojure.contrib.command-line]))
+
+(defn -main [& args]
+  (with-command-line args
+      "Command line demo"
+      [[foo "This is the description for foo" 1]
+       [bar "This is the description for bar" 2]
+       [boolean? b? "This is a boolean flag."]
+       remaining]
+    (println "foo: " foo)
+    (println "bar: " bar)
+    (println "boolean?: " boolean?)
+    (println "remaining: " remaining)))
+
+

eb54b128a13bcf5368ebd14ecc033dab53b82b8e
rmckenzie <rmckenzie92@gmail.com>
2012-07-23 13:25:44 -0500

    Initial lein state

---
+++ src/doubletake/core.clj
@@ -0,0 +1,6 @@
+(ns doubletake.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/doubletake/core_test.clj
@@ -0,0 +1,7 @@
+(ns doubletake.core-test
+  (:use clojure.test
+        doubletake.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

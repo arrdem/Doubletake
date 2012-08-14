ece586869f7e877bd9fdf8a30f64c544f741c91d
rmckenzie <rmckenzie92@gmail.com>
2012-08-14 09:48:33 -0500

    Patch and require the Eclipse library JAR

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject doubletake "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A change highlighting tool for Java which uses changes in the AST resulting from edits to project semantic interference"
+  :url ""
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.eclipse/sdk "0.1.0"]
+                 [org.eclipse/sdk "0.1.1"]
                  ])
